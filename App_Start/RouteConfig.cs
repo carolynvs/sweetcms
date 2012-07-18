6e60d0f165144bddbbbba561e08584752d09424b
carolynvs <me@carolynvanslyck.com>
2012-07-18 18:02:23 -0500

    Support nested routes and removed bins again

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace SweetCMS
@@ -15,31 +9,11 @@ namespace SweetCMS
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-
             routes.MapRoute(
-            name: "Dynamic",
-            url: "{view}",
-            constraints: new { view = GetAvailableViews() },
-            defaults: new { controller = "Home", action = "RenderPage" }
+                name: "CMS",
+                url: "{*path}",
+                defaults: new {controller = "Home", action = "RenderView"}
                 );
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-        }
-
-        private static string GetAvailableViews()
-        {
-            string viewsFolder = HttpContext.Current.Server.MapPath("~/Views/Home");
-            IEnumerable<string> views = Directory.EnumerateFiles(viewsFolder).Select(Path.GetFileNameWithoutExtension);
-            return string.Join("|", views);
         }
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
 
 namespace SweetCMS.Controllers
 {
@@ -9,9 +12,43 @@ namespace SweetCMS.Controllers
             return View();
         }
 
-        public ActionResult RenderPage(string view)
+        public ActionResult RenderView(string path)
         {
+            IView view = GetView(path);
+
+            if (view == null)
+            {
+                throw new HttpException((int)HttpStatusCode.NotFound, string.Format("The page {0} was not found", view));
+            }
+
             return View(view);
+        }
+
+        public IView GetView(string path)
+        {
+            path = path ?? "Index";
+
+            IView view = ResolveView(path);
+
+            if (view != null)
+                return view;
+
+            if (path.ToLower().EndsWith("index"))
+                return null;
+
+            return ResolveView(path + "/Index");
+        }
+
+        private IView ResolveView(string viewPath)
+        {
+            try
+            {
+                return ViewEngines.Engines.FindView(ControllerContext, viewPath, null).View;
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
